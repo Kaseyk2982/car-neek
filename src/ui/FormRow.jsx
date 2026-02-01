21d7a854430b85c5e50c48f49f5a49ccb73051f0
Kasey Kovachevich <kaseyk2982@gmail.com>
2026-02-01 16:36:32 -0700

    optimized sale form for mobile

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,11 +1,11 @@
 export default function FormRow({ error, label, children }) {
   return (
-    <div className="grid items-center grid-cols-[24rem_1fr_1fr] gap-6 py-5 px-0 first:pt-0 last:pb-0">
+    <div className="grid grid-cols-1 gap-3 py-4 px-0 first:pt-0 last:pb-0 sm:gap-6 sm:py-5 sm:items-center sm:grid-cols-[24rem_1fr_1fr]">
       <label className="font-semibold" htmlFor={children.props.id}>
         {label}
       </label>
       {children}
-      <span className="text-xl text-red-700">{error}</span>
+      <span className="text-sm text-red-700 sm:text-xl">{error}</span>
     </div>
   );
 }
